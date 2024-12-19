8add08c81e59903ccb54d585f748293e9151a1ea
657 <kuyermqi@outlook.com>
2024-12-20 00:14:00 +0800

    init

---
+++ extension.js
@@ -0,0 +1,203 @@
+import GObject from 'gi://GObject'
+import St from 'gi://St'
+import Soup from 'gi://Soup'
+import GLib from 'gi://GLib'
+import Gio from 'gi://Gio'
+
+import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js'
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js'
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js'
+import * as Main from 'resource:///org/gnome/shell/ui/main.js'
+
+let settings = null
+
+class Indicator extends PanelMenu.Button {
+  static {
+    GObject.registerClass(this)
+  }
+
+  _init() {
+    super._init(0.0, _('Mihomo Tray'))
+
+    if (settings !== null) {
+      this._externalController = settings.get_string('external-controller') || null
+      this._metacubexdUrl = settings.get_string('metacubexd-url') || null
+      settings.connect('changed::external-controller', (settings, key) => {
+        this._indicator._externalController = settings.get_string(key) || null
+      })
+      settings.connect('changed::metacubexd-url', (settings, key) => {
+        this._indicator._metacubexdUrl = settings.get_string(key) || null
+      })
+    }
+
+    const prefItem = new PopupMenu.PopupMenuItem('Preferences')
+    prefItem.connect('activate', () => this.openPrefs())
+    this.menu.addMenuItem(prefItem)
+
+    const separator = new PopupMenu.PopupSeparatorMenuItem()
+    this.menu.addMenuItem(separator)
+
+    const openMetacubeXDItem = new PopupMenu.PopupMenuItem('Open MetacubeXD')
+    openMetacubeXDItem.connect('activate', () => this._openMetacubeXD())
+    this.menu.addMenuItem(openMetacubeXDItem)
+
+    const reloadItem = new PopupMenu.PopupMenuItem('Reload Configuration')
+    reloadItem.connect('activate', () => this._reloadConfiguration())
+    this.menu.addMenuItem(reloadItem)
+
+    this._httpSession = new Soup.Session()
+    this._fetchConfigs()
+  }
+
+  _fetch({ method, url, resolve, reject = console.error, body = null }) {
+    const message = Soup.Message.new(method, url)
+
+    if (body !== null) {
+      const bytes = new TextEncoder().encode(JSON.stringify(body))
+      message.set_request_body_from_bytes('application/json', new GLib.Bytes(bytes))
+    }
+
+    this._httpSession.send_and_read_async(message, GLib.PRIORITY_DEFAULT, null, (session, result) => {
+      try {
+        const bytes = session.send_and_read_finish(result)
+        const response = new TextDecoder('utf-8').decode(bytes)
+        resolve(response)
+      } catch (err) {
+        reject(err)
+      }
+    })
+  }
+
+  _fetchConfigs() {
+    if (!this._externalController) {
+      console.error('\n\external controller is null\n\n')
+      return
+    }
+
+    this._fetch({
+      method: 'GET',
+      url: `${this._externalController}/configs`,
+      resolve: res => {
+        const configs = JSON.parse(res)
+        const items = []
+
+        if (configs.hasOwnProperty('allow-lan')) {
+          const item = new PopupMenu.PopupMenuItem('Allow Lan: ' + (configs['allow-lan'] ? 'ON' : 'OFF'))
+          const onAllowLanChanged = value => {
+            item.label.text = 'Allow Lan: ' + (value ? 'ON' : 'OFF')
+            configs['allow-lan'] = value
+          }
+          item.connect('activate', () => this._changeAllowLan(!configs['allow-lan'], onAllowLanChanged))
+          items.push(item)
+        }
+
+        if (configs.hasOwnProperty('mode')) {
+          const item = new PopupMenu.PopupSubMenuMenuItem('Mode: ' + configs['mode'].toUpperCase())
+          const onModeChanged = newMode => { item.label.text = 'Mode: ' + newMode }
+          item.menu.addAction('GLOBAL', () => this._changeMode('Global', onModeChanged))
+          item.menu.addAction('RULE', () => this._changeMode('Rule', onModeChanged))
+          item.menu.addAction('DIRECT', () => this._changeMode('Direct', onModeChanged))
+          items.push(item)
+        }
+
+        if (items.length !== 0) {
+          const separator = new PopupMenu.PopupSeparatorMenuItem()
+          this.menu.addMenuItem(separator)
+        }
+
+        items.forEach(item => this.menu.addMenuItem(item))
+      },
+      reject: err => {
+        console.error(`\n\n${err}\n\n`)
+        Main.notify('Mihomo Tray', 'Failed to fetch configs, please check the status of mihomo service.')
+      }
+    })
+  }
+
+  _openMetacubeXD() {
+    if (!this._metacubexdUrl) {
+      console.error('\n\nmetacubexd url is null\n\n')
+      return
+    }
+    try {
+      Gio.AppInfo.launch_default_for_uri_async(this._metacubexdUrl, null, null, () => { })
+    } catch (e) {
+      console.error(`${e}`)
+    }
+  }
+
+  _changeMode(mode, onSuccess) {
+    if (this._externalController === null) {
+      console.error('\n\external controller is null\n\n')
+      return
+    }
+    this._fetch({
+      method: 'PATCH',
+      url: `${this._externalController}/configs`,
+      body: { mode },
+      resolve: () => {
+        Main.notify('Mihomo Tray', `Proxy mode changed to ${mode}.`)
+        onSuccess(mode)
+      }
+    })
+  }
+
+  _changeAllowLan(allowLan, onSuccess) {
+    if (this._externalController === null) {
+      console.error('\n\external controller is null\n\n')
+      return
+    }
+    this._fetch({
+      method: 'PATCH',
+      url: `${this._externalController}/configs`,
+      body: { 'allow-lan': allowLan },
+      resolve: () => {
+        Main.notify('Mihomo Tray', `Allow Lan: ${allowLan ? 'ON' : 'OFF'}`)
+        onSuccess(allowLan)
+      }
+    })
+  }
+
+  _reloadConfiguration() {
+    this._fetch({
+      method: 'PUT',
+      url: `${this._externalController}/configs`,
+      resolve: () => {
+        Main.notify('Mihomo Tray', `Configuration reloded.`)
+      },
+      reject: (err) => {
+        Main.notify('Mihomo Tray', `Failed to relod configuration.`)
+      },
+    })
+  }
+
+  openPrefs() {}
+
+  destroyHttpSession() {
+    if (this._httpSession) {
+      this._httpSession.abort()
+      this._httpSession = null
+    }
+  }
+}
+
+export default class IndicatorExampleExtension extends Extension {
+  enable() {
+    settings = this.getSettings('org.gnome.shell.extensions.mihomotray')
+    
+    const clashIcon = new St.Icon({ gicon: Gio.icon_new_for_string(this.metadata.path + '/clash.svg') })
+    clashIcon.set_icon_size(16)
+    this._indicator = new Indicator()
+    this._indicator.add_child(clashIcon)
+    this._indicator.openPrefs = () => this.openPreferences()
+
+    Main.panel.addToStatusArea(this.uuid, this._indicator)
+  }
+
+  disable() {
+    this._indicator.destroyHttpSession()
+    this._indicator.destroy()
+    this._indicator = null
+    settings = null
+  }
+}
---
+++ prefs.js
@@ -0,0 +1,38 @@
+import Gio from 'gi://Gio'
+import Adw from 'gi://Adw'
+
+import { ExtensionPreferences } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js'
+
+export default class MihomoTrayPreferences extends ExtensionPreferences {
+  fillPreferencesWindow(window) {
+    const page = new Adw.PreferencesPage({
+      title: 'Mihomo Tray Perferences',
+      icon_name: 'dialog-information-symbolic',
+    })
+    window.add(page)
+
+    const group0 = new Adw.PreferencesGroup()
+    page.add(group0)
+    const input0 = new Adw.EntryRow({ title: 'External Controller' })
+    group0.add(input0)
+    const btn0 = new Adw.ButtonRow({ title: 'Reset' })
+    btn0.connect('activated', () => {
+      window._settings.set_string('external-controller', 'http://localhost:9090')
+    })
+    group0.add(btn0)
+
+    const group1 = new Adw.PreferencesGroup()
+    page.add(group1)
+    const input1 = new Adw.EntryRow({ title: 'MetacubeXD URL' })
+    group1.add(input1)
+    const btn1 = new Adw.ButtonRow({ title: 'Reset' })
+    btn1.connect('activated', () => {
+      window._settings.set_string('metacubexd-url', 'https://d.metacubex.one')
+    })
+    group1.add(btn1)
+
+    window._settings = this.getSettings('org.gnome.shell.extensions.mihomotray')
+    window._settings.bind('external-controller', input0, 'text', Gio.SettingsBindFlags.DEFAULT)
+    window._settings.bind('metacubexd-url', input1, 'text', Gio.SettingsBindFlags.DEFAULT)
+  }
+}
